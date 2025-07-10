328e4014a5dc30a14925c6d88dff23a0bcf7e7f9
Ismail Lyamani <ismaillyamani4@gmail.com>
2025-07-10 12:34:48 +0100

    Moyenne de chaque etudiant

--- excel_handler.py
+++ excel_handler.py
@@ -2,14 +2,44 @@ from openpyxl import load_workbook, Workbook
 import os
 import pandas as pd
 
-class ExcelHandler:
+class GradeProcessor:
 
-    @staticmethod
-    def read_excel(file_path):
+    def __init__(self,input_file):
+        self.input_file = input_file
+        self.subjects = ["POO", "MOO", "WEB", "THL", "Linux", "LE1", "CAS"]
+        self.df = None
 
-        if not os.path.exists(file_path):
-            print(f"File not found: {file_path}")
+    def read_excel(self):
+        try:
+            self.df = pd.read_excel(self.input_file)
+            print("File read successfully")
+        except Exception as e:
+            print(f"Error while reading file: {e}")
 
-        return pd.read_excel(file_path)
+    def calculate_average (self):
+        if self.df is not None:
+            self.df["Moyenne"] = self.df[self.subjects].mean(axis=1).round(2)
+            print ("average calculated successfully")
+            print(self.df)
 
+    def verification(self):
+        print("verification...")
+        if self.df is not None:
+            if self.df["Moyenne"] >= 12:
+                self.df["Résultat"] = 'V'
+            else:
+                self.df["Résultat"] = 'R'
+        print(self.df)
+
+    def process(self):
+        self.read_excel()
+        self.calculate_average()
+        self.verification()
+
+
+if __name__ == "__main__":
+    processor = GradeProcessor(
+        input_file="notes.xlsx",
+    )
+    processor.process()
 
